a71c28034ec13b232079e503232454c589acdca1
sandeep <jena.sandp@gmail.com>
2022-06-28 15:44:20 +0530

    requirements and setup.py was added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os
+
+setup(
+    name="src",
+    version="0.0.1",
+    # description="invisible you in video without any proof",
+    # # url="https://github.com/sandeepjena7",
+    # author="sandeepjean777",
+    # author_email="sandeepjean777@gmail.com",
+    packages=["src"],
+    python_requires=">=3.6",
+    install_requires=[
+
+    ]
+    )
